b0f78f7e9098a095826f25fe2d2d30b45973a060
uma-sketch <main.vertex5@gmail.com>
2026-02-10 15:04:46 +0530

    Update network.tf

--- network.tf
+++ network.tf
@@ -6,21 +6,21 @@ resource "azurerm_resource_group" "rg" {
 resource "azurerm_virtual_network" "vnet" {
   name                = "vnet-demo"
   address_space       = ["10.0.0.0/16"]
-  location            = var.location
-  resource_group_name = LBRG1
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet-web"
-  resource_group_name  = LBRG1
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_security_group" "nsg" {
   name                = "nsg-web"
-  location            = var.location
-  resource_group_name = LBRG1
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
     name                       = "AllowHTTP"
